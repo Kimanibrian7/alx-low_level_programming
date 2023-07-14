bc6fa859bb198cc3e9833b76e19bd8416abf3a1d
kimani7 <kimani.brian.777@gmail.com>
2023-07-14 09:53:09 -0700

    Digits

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ *
+ * Return: return 0 if successful
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
