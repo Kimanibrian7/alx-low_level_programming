69a37e3d378ab091725042c328588259f44db4ea
kimani7 <kimani.brian.777@gmail.com>
2023-07-19 07:27:35 -0700

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests if test is negative or positive
+ *
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
