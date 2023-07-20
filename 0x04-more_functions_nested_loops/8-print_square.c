542460d8b7907a4b607dfea2cd5e1e9661e8d96c
kimani7 <kimani.brian.777@gmail.com>
2023-07-20 08:25:02 -0700

    all C more functions projects

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,15 +10,15 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (n > 0)
+	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
-				putchar('#');
-			putchar('\n');
+				_putchar('#');
+			_putchar('\n');
 		}
 	}
 	else
-		putchar('\n');
+		_putchar('\n');
 }
