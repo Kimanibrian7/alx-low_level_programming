a31ffba9dca111921098dc456179e87463db6617
kimani7 <kimani.brian.777@gmail.com>
2023-07-20 22:31:25 -0700

    resolving the 10 triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,16 +10,21 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; i++)
+	if (size > 0)
 	{
-		for (j = size -i; j > 0; j--)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar(' ');
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
 		}
-		for (j = 0; j < i; j++)
-		{
-			_putchar('#');
-		}
-	_putchar('\n');
 	}
+	else
+		_putchar('\n');
 }
