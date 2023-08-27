66e81ef21ecd4120f925c12a90d755448b0dfa69
kimani B <kimani.brian.777@gmail.com>
2023-08-27 10:25:41 -0700

    all variadic function tasks

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ * @separator: pointer to a separator string
+ * @n: number of arguments
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n)
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator && i < (n -1))
+			printf("%s", seperator);
+	}
+	va_end(args);
+	printf("\n");
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_function.h"
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings to stardard output
+ * @separator: string pointer to separator
+ * @n: number of arguments
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *s
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(args, char *);
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s", s);
+		if (separator && i < (n - 1))
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
